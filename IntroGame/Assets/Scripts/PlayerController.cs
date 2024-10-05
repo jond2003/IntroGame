469a453ae21fdb60b957ed6a5adac5220e11ee86
Jaden Bellevue <68704634+jaebel@users.noreply.github.com>
2024-10-05 20:56:50 +0100

    Add rotation and distance features
    4.2.3 completed

--- IntroGame/Assets/Scripts/PlayerController.cs
+++ IntroGame/Assets/Scripts/PlayerController.cs
@@ -13,12 +13,17 @@ public class PlayerController : MonoBehaviour
     private int numPickups = 3;
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI winText;
+    public TextMeshProUGUI playerPosition;
+    public TextMeshProUGUI playerVelocity;
+    private Vector3 previousPosition;
+    private Vector3 velocity;
 
     void Start()
     {
         count = 0;
         winText.text = " ";
         SetCountText();
+        previousPosition = transform.position;
     }
 
     void OnMove(InputValue value)
@@ -30,6 +35,20 @@ public class PlayerController : MonoBehaviour
     {
         Vector3 movement = new Vector3(moveValue.x, 0.0f, moveValue.y);
         GetComponent<Rigidbody>().AddForce(movement * speed * Time.fixedDeltaTime);
+
+        // Update the player position
+        playerPosition.text = "Position: " + transform.position.ToString("F2");
+
+        // Velocity = difference in position divided by time
+        velocity = (transform.position - previousPosition) / Time.fixedDeltaTime;
+
+        // Update previous position
+        previousPosition = transform.position;
+
+        // Update the player velocity and speed on the display
+        float speedScalar = velocity.magnitude; // Speed as a scalar = magnitude of velocity
+        playerVelocity.text = "Velocity: " + velocity.ToString("F2") +
+                              "\nSpeed: " + speedScalar.ToString("F2") + " units/second";
     }
 
     void OnTriggerEnter(Collider other)
